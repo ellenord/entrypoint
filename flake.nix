601fb1e2e123b2cae44ba0113d0085706403a913
ellenord <ellenord.zelleratumm@gmail.com>
2025-07-20 14:44:12 +0300

    asdgvs

--- flake.nix
+++ flake.nix
@@ -32,8 +32,7 @@
           nixfmt
         ];
         shellHook = ''
-          export NIX_CONFIG=$'experimental-features = nix-command flakes\nallow-unsafe-native-code-during-evaluation = true'
-          export NIXPKGS_ALLOW_UNFREE=1
+
         '';
       };
     }
---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export NIX_CONFIG=$'experimental-features = nix-command flakes\nallow-unsafe-native-code-during-evaluation = true'
+export NIXPKGS_ALLOW_UNFREE=1
+
+set -a
+source setup.env
+set +a
